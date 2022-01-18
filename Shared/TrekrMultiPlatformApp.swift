a1a5115fe465f03c31906a998ca4d0bbe7ef1f8c
Yeseo Kim <YKim@lcs.on.ca>
2022-01-18 09:22:14 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Yeseo Kim on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
