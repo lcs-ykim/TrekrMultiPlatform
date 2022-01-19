1de321ae6c7e31ad4da047fe3c900aecab6a99f4
Yeseo Kim <YKim@lcs.on.ca>
2022-01-19 10:12:44 -0500

    Added NavigationLink to the map view. This makes the map clickable!

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -24,11 +24,16 @@ struct WorldMap: View {
             
             MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
                 
-                Image(location.country)
-                    .resizable()
-                    .cornerRadius(10)
-                    .frame(width: 80, height: 40)
-                    .shadow(radius: 3)
+                NavigationLink(destination: LocationDetail(location: location)) {
+                    
+                    Image(location.country)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width: 80, height: 40)
+                        .shadow(radius: 3)
+
+                }
+                
             }
             
         }
