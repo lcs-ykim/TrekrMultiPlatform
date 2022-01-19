0ff7ada2250410943a02e04dea0c68f7c23c3c8d
Yeseo Kim <YKim@lcs.on.ca>
2022-01-18 19:57:19 -0500

    Added tips and map view, and tab bar to switch between them

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Yeseo Kim on 2022-01-18.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello, TipsList View!")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Yeseo Kim on 2022-01-18.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, WorldMap View!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
