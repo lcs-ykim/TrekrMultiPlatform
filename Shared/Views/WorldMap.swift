61d830d34d061e30446deed15ee41b55ceaac810
Yeseo Kim <YKim@lcs.on.ca>
2022-01-19 10:01:17 -0500

    Got map view centred on North American eastern seaboard

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,16 +5,29 @@
 //  Created by Yeseo Kim on 2022-01-18.
 //
 
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    // Centre on LCS, wide enough to show most of eastern North America
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922,
+                                                                          longitude: -78.26571),
+                                           span: MKCoordinateSpan(latitudeDelta: 40,
+                                                                  longitudeDelta: 40))
     var body: some View {
-        Text("Hello, WorldMap View!")
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
+        
     }
 }
 
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
-        WorldMap()
+        
+        NavigationView {
+            WorldMap()
+        }
     }
 }
